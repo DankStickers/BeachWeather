6eb6b0f373dffd6b5cec3f8542dccfcae0b2dbba
brandon-harris <branbharris@gmail.com>
2015-04-18 22:35:30 -0400

    PHP stuff uploaded
    php scripts bradley did or whatever.

--- getWeather.php
+++ getWeather.php
@@ -1,7 +1,12 @@
 <?php
 
-$APIKEY = "d7170ab8a71f4a9db07c39384a1633f3";
-
-$weather = http_get("api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID", APPID=APIKEY);
-
-echo $weather
+$json_string = file_get_contents("http://api.openweathermap.org/data/2.5/forecast/city?id=4500092&d7170ab8a71f4a9db07c39384a1633f3");
+$parsed_json = json_decode($json_string);
+$temperature = $parsed_json->{'list'}['0']->main->temp;
+/*print_r($parsed_json);*/
+$ftemp = (($temperature - 273.15) * 1.8)+32;
+print_r($temperature);
+print_r("\n");
+print_r($ftemp);
+print_r("\n");
+?>
